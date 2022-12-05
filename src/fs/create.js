ed9c99473eff0f70f1c721e1d3d43aeb6295a474
arminemarti <91451587+arminemarti@users.noreply.github.com>
2022-12-05 13:53:07 +0400

    Update create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,14 @@
 const create = async () => {
-    // Write your code here 
+    const fs = require('fs');
+
+	const content = 'I am fresh and young.';
+
+	fs.writeFile('fresh.txt', content, err => {
+	  if (err) {
+		console.error('FS operation failed.');	
+	  }  
+	});
+	console.log('The file written successfully.');
 };
 
-await create();
+create();
